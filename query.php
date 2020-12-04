01e8f88025d25f226dee314d9573fb0a43d8fbd6
John James Jacoby <johnjamesjacoby@me.com>
2020-12-04 14:19:16 -0600

    Nulls: validate null values if allowed, on any column.
    Up until now, the "Column::allow_null" flag was only partially implemented, working sometimes for "datetime" columns, but not for any others.
    
    This commit ensures that, in the "Query::validate_item" method, null values for all item keys are allowed when "allow_null" is true for that column, and returns false early when attempting to save a null value to an item key when it is not allowed.
    
    This commit also removes the special datetime, column type, null value handling that was added as part of #22, since it is now explicitly globally allowed (when allowed) in the global validator.
    
    This commit also modifies "Column::sanitize_default" to only allow null defaults when "allow_null" is true, adds explicit support for strings and integers, and adds a @todo regarding other column types that may have their own unique default values (datetime, decimal, timestamp, and others come to mind).
    
    Fixes #76.

--- query.php
+++ query.php
@@ -8,7 +8,7 @@
  * @license     https://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0.0
  */
-namespace BerlinDB\Database;
+namespace Sugar_Calendar\Database;
 
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
@@ -1984,21 +1984,25 @@ class Query extends Base {
 		// Loop through item attributes
 		foreach ( $item as $key => $value ) {
 
-			// Only allow null if column allows null
-			if ( is_null( $value ) && $this->get_column_field( array( 'name' => $key ), 'allow_null' ) ) {
-				$value = null;
-
 			// Strip slashes from all strings
-			} elseif ( is_string( $value ) ) {
+			if ( is_string( $value ) ) {
 				$value = stripslashes( $value );
 			}
 
-			// Get callback for column
-			$callback = $this->get_column_field( array( 'name' => $key ), 'validate' );
+			// Get column
+			$column = $this->get_column_by( array( 'name' => $key ) );
+
+			// Null value is special for all item keys
+			if ( is_null( $value ) ) {
+
+				// Bail if null is not allowed
+				if ( false === $column->allow_null ) {
+					return false;
+				}
 
 			// Attempt to validate
-			if ( ! empty( $callback ) && is_callable( $callback ) ) {
-				$validated = call_user_func( $callback, $value );
+			} elseif ( ! empty( $column->callback ) && is_callable( $column->callback ) ) {
+				$validated = call_user_func( $column->callback, $value );
 
 				// Bail if error
 				if ( is_wp_error( $validated ) ) {
