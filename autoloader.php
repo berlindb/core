12bb31c232186379067b6550c9a5928ee171e598
John James Jacoby <johnjamesjacoby@me.com>
2022-06-18 20:17:50 -0500

    Autoloader: minor code cleanup

--- autoloader.php
+++ autoloader.php
@@ -24,7 +24,7 @@ spl_autoload_register(
 
 		// Project namespace & length.
 		$root_namespace    = 'BerlinDB\\';
-		$project_namespace = 'BerlinDB\\Database\\';
+		$project_namespace = $root_namespace . 'Database\\';
 		$length            = strlen( $project_namespace );
 
 		// Bail if class is not in this namespace.
@@ -33,11 +33,12 @@ spl_autoload_register(
 		}
 
 		// Setup file parts.
-		$format = '%1$s/src/%2$s.php';
-		$path   = __DIR__;
-		$name   = str_replace( '\\', DIRECTORY_SEPARATOR, str_replace( $root_namespace, '', $class_name ) );
+		$strip  = str_replace( $root_namespace, '', $class_name );
+		$name   = str_replace( '\\', DIRECTORY_SEPARATOR, $strip );
 
 		// Parse class and namespace to file.
+		$format = '%1$s/src/%2$s.php';
+		$path   = __DIR__;
 		$file   = sprintf( $format, $path, $name );
 
 		// Bail if file does not exist.
